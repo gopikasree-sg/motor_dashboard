5c8aaebf60c313c851c032477dfd25c7b824b553
gopikasree-sg <gopikasree.26ee@licet.ac.in>
2026-03-06 11:08:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,18 @@ app = Flask(__name__)
 CHANNEL1_ID = "3271158"
 CHANNEL2_ID = "3273029"
 CHANNEL3_ID = "3285406"
+
 READ_API_KEY_1 = "BUYHL98SRGZD2OK4"
 READ_API_KEY_2 = "2DLTEU46Z7R2KXIS"
 READ_API_KEY_3 = "S1TBFQP66JVMJGSF"
 
+
 def get_last_hour(channel_id, read_key):
     url = f"https://api.thingspeak.com/channels/{channel_id}/feeds.json?api_key={read_key}&results=60"
     response = requests.get(url)
-    return response.json()["feeds"]
+    data = response.json()
+    return data["feeds"]
+
 
 @app.route("/")
 def dashboard():
@@ -24,16 +28,18 @@ def dashboard():
     mechanical = get_last_hour(CHANNEL2_ID, READ_API_KEY_2)
     analytics = get_last_hour(CHANNEL3_ID, READ_API_KEY_3)
 
-    return render_template("index.html",
-                           electrical=electrical,
-                           mechanical=mechanical,
-                           analytics=analytics)
+    return render_template(
+        "index.html",
+        electrical=electrical,
+        mechanical=mechanical,
+        analytics=analytics
+    )
 
-# CSV Download Route
+
 @app.route("/download")
 def download_csv():
 
-    analytics = get_last_hour(CHANNEL3_ID)
+    analytics = get_last_hour(CHANNEL3_ID, READ_API_KEY_3)
 
     output = io.StringIO()
     writer = csv.writer(output)
@@ -43,18 +49,21 @@ def download_csv():
     for row in analytics:
         writer.writerow([
             row["created_at"],
-            row["field1"],
-            row["field2"],
-            row["field5"],
-            row["field8"]
+            row.get("field1"),
+            row.get("field2"),
+            row.get("field5"),
+            row.get("field8")
         ])
 
     output.seek(0)
 
-    return send_file(io.BytesIO(output.getvalue().encode()),
-                     mimetype="text/csv",
-                     as_attachment=True,
-                     download_name="last_1_hour_data.csv")
+    return send_file(
+        io.BytesIO(output.getvalue().encode()),
+        mimetype="text/csv",
+        as_attachment=True,
+        download_name="last_1_hour_data.csv"
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
